d0fb07c1071c103e220664bd847d7fc9aa49587e
timpak <timothy.pak@liferay.com>
2019-10-18 11:58:22 -0700

    Revert "LPS-97561 portal-search-test-util: Ignore relevance for tests where scores are equal"
    This reverts commit 2e2483f6ba21e1466b0bc8d397bae05ec4e5423c.

--- modules/apps/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/legacy/query/BaseStringQueryTestCase.java
+++ modules/apps/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/legacy/query/BaseStringQueryTestCase.java
@@ -157,7 +157,7 @@ public abstract class BaseStringQueryTestCase extends BaseIndexingTestCase {
 				indexingTestHelper.search();
 
 				indexingTestHelper.verify(
-					hits -> DocumentsAssert.assertValuesIgnoreRelevance(
+					hits -> DocumentsAssert.assertValues(
 						indexingTestHelper.getRequestString(), hits.getDocs(),
 						_FIELD_NAME, expectedValues));
 			});
